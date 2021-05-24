259cbfbbe00f80d8ca7967dbed2e537b01b9571b
huang <1091494437@qq.com>
2021-05-24 23:43:03 +0800

    Learn_V1.0

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Hashtable ht = new Hashtable(); 
+            ArrayList aList = new ArrayList();
+            List<string> bList = new List<string>();
+            aList.Add("a");
+            aList.Add("b");
+            aList.Add("c");
+            aList.Add("d");
+            aList.Add("e");
+            aList.Insert(3,"123456");
+            foreach (var item in aList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
